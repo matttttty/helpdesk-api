b72ee03eb3f32e1a063ae907bdea90c111814fd4
Matty H <mat.honcharenko@yahoo.com>
2026-03-08 10:25:19 +0200

    feat: add database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"helpdesk-api/internal/repository"
+	"log"
+)
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("Helpdesk API")
+	db, err := repository.NewDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+	fmt.Println("Connected to database", db.Stats())
 }
